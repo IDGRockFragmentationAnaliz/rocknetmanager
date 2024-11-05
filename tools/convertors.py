e8a24f0592b81259af182a5294b786add0719c88
kolad <koladik@gmail.com>
2024-11-05 21:59:09 +0300

    module update

--- tools/convertors.py
+++ tools/convertors.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
+import cv2
 import numpy as np
 import rasterio
+from rasterio.merge import merge
 
 
 def geotif2geotif_standart(input_path: Path, output_folder: Path):
@@ -20,16 +22,53 @@ def geotif2geotif_standart(input_path: Path, output_folder: Path):
 		) as dst:
 			dst.write(image)
 
+def put_labels(geo_mosaic, geo_parts, geo_paths):
+	np_mosaic = geo_mosaic.read()
+	print(geo_mosaic.meta)
+	np_mosaic = np.transpose(np_mosaic, (1, 2, 0))
+	np_mosaic = np.ascontiguousarray(np_mosaic)
 
-def mosaic_merge(images_folder: Path, save_path='mosaic.tif'):
+	bounds = geo_mosaic.bounds
+	dx = (bounds.right - bounds.left)
+	dy = (bounds.top - bounds.bottom)
+	print(np_mosaic.shape)
+	print(dx / 0.0005)
+	print(dy / 0.0005)
+
+	bb = geo_parts[0].bounds
+	center_x = (bb.left + bb.right) / 2 - bounds.left
+	center_y = (bb.top + bb.bottom) / 2 - bounds.bottom
+
+	col = int(center_x / 0.0005)
+	row = -int(center_y / 0.0005)
+	print(col, row)
+	cv2.putText(
+		np_mosaic,
+		geo_paths[0].stem,
+		(col, row),
+		cv2.FONT_HERSHEY_SIMPLEX,
+		5,
+		(0, 0, 255),
+		2,
+		cv2.LINE_AA
+	)
+	np_mosaic = np.transpose(np_mosaic, (2, 0, 1))
+	geo_mosaic.write(np_mosaic)
+
+
+
+def mosaic_merge(images_folder: Path, save_path='mosaic.tif', labels=False):
 	path_images = [path for path in images_folder.iterdir()]
 	geo_images = [rasterio.open(f) for f in path_images]
 	mosaic, out_trans = merge(geo_images)
+
+
 	out_meta = geo_images[0].meta.copy()
 	out_meta.update({
 		"height": mosaic.shape[1],
 		"width": mosaic.shape[2],
 		"transform": out_trans
 	})
-	with rasterio.open(save_path, 'w', **out_meta) as dest:
-		dest.write(mosaic)
+	with rasterio.open(save_path, 'r+', **out_meta) as geo_mosaic:
+		geo_mosaic.write(mosaic)
+		put_labels(geo_mosaic, geo_images, path_images)
