d8a90b016c3deb394a920c0e6d36268c8a01abaa
Kolad2 <koladik@gmail.com>
2024-11-16 06:25:58 +0300

    update

--- tools/vectorize.py
+++ tools/vectorize.py
@@ -1,40 +1,18 @@
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 import sknw
+from pathlib import Path
 from shapely.geometry import LineString
 import shapefile
 
 
-def vectorize2(image_thin):
-	image_thin = cv2.ximgproc.thinning(image_thin)
-	contours, _ = cv2.findContours(image_thin, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
-
-	polygons = []
-	for contour in contours:
-		poly = contour.squeeze(axis=1)
-		if len(poly) > 2:
-			polygons.append(poly)
-
-	fig = plt.figure(figsize=(7, 9))
-	axs = [fig.add_subplot(1, 1, 1)]
-	axs[0].imshow(cv2.merge((image_thin, image_thin, image_thin)))
-
-	for poly in polygons:
-		axs[0].plot(poly[:, 0], poly[:, 1], '.-', color="blue")
-	plt.show()
-
-
-def shpline_save(path, polylines):
-	with shapefile.Writer(path, shapefile.POLYLINE) as shp:
+def shpline_save(path: Path, polylines):
+	path.mkdir(parents=False, exist_ok=True)
+	with shapefile.Writer(path / (path.stem + ".shp"), shapefile.POLYLINE) as shp:
 		shp.field("NAME", "C")
 		for polyline in polylines:
 			shp.line([(polyline * [1, -1]).tolist()])
 			shp.record("Polyline")
-#poly = LineString(eds['pts'])
-		#poly = poly.simplify(1)
-		#poly = poly.coords
-		#poly = np.flip(np.array(poly))
 
 
 def graph_to_lines(graph):
@@ -42,41 +20,56 @@ def graph_to_lines(graph):
 	for (s, e, key, data) in graph.edges(keys=True, data=True):
 		eds = graph[s][e][key]
 		poly = eds['pts']
+		poly = LineString(poly)
+		poly = poly.simplify(1)
+		poly = poly.coords
+		poly = np.fliplr(np.array(poly))
 		polylines.append(poly)
+
 	return polylines
 
+
 def filer_corners(image_thin):
-	filter_image = (image_thin.copy()/255).astype(np.int16)
+	filter_image = (image_thin.copy() / 255).astype(np.int16)
 	kernel = np.array([
 		[0, 1, 0],
 		[0, 10, 1],
 		[-1, 0, 0]
 	], dtype=np.int16)
-	filtered = cv2.filter2D(filter_image, -1, kernel)
-	mask = (filtered >= 12)
+	mask = (cv2.filter2D(filter_image, -1, kernel) >= 12)
 	image_thin[mask] = 0
-	print(np.sum(mask))
+
+	kernel = np.array([
+		[-1, 0, 0],
+		[0, 10, 1],
+		[0, 1, 0]
+	], dtype=np.int16)
+	mask = (cv2.filter2D(filter_image, -1, kernel) >= 12)
+	image_thin[mask] = 0
+
+	kernel = np.array([
+		[0, 0, -1],
+		[1, 10, 0],
+		[0, 1, 0]
+	], dtype=np.int16)
+	mask = (cv2.filter2D(filter_image, -1, kernel) >= 12)
+	image_thin[mask] = 0
+
+	kernel = np.array([
+		[0, 1, 0],
+		[1, 10, 0],
+		[0, 0, -1]
+	], dtype=np.int16)
+	mask = (cv2.filter2D(filter_image, -1, kernel) >= 12)
+	image_thin[mask] = 0
+
 	return image_thin
 
-def vectorize(image_thin):
+
+def vectorize(image_thin, save_folder: Path):
 	image_thin = cv2.ximgproc.thinning(image_thin)
 	image_thin = filer_corners(image_thin)
-
 	graph = sknw.build_sknw(image_thin, multi=True)
 	polylines = graph_to_lines(graph)
-
-	fig = plt.figure(figsize=(7, 5))
-	axs = [fig.add_subplot(1, 2, 1),
-	       fig.add_subplot(1, 2, 2)]
-	axs[0].imshow(image_thin, cmap='gray')
-	axs[1].imshow(image_thin, cmap='gray')
-	for polyline in polylines:
-		axs[1].plot(polyline[:,1], polyline[:,0])#, color="blue"
-	plt.show()
-
-	#shpline_save("shape/test.shp", polylines)
-
-
-
-
-
+	shpline_save(save_folder, polylines)
+	return polylines
