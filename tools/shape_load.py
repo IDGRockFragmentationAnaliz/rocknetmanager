c4406cb547411322b2c2ab091dfb3b5d03aec26a
Kolad2 <koladik@gmail.com>
2024-11-17 03:17:02 +0300

    update 1

---
+++ tools/shape_load.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import sknw
+from pathlib import Path
+from shapely.geometry import LineString
+import shapefile
+from typing import List
+
+
+def shape_lines_save(path: Path):
+	with shapefile.Reader(path) as shp:
+		shapes = shp.shapes()
+		bbox = np.array(shp.bbox, np.float32)
+		lines = []
+		for shape in shapes:
+			line = np.array(shape.points, np.float32) * [1, -1]
+			lines.append(line)
+		return lines, bbox
